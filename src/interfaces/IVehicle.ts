1150992c7bef682937dfc3d24e681eaadd2d38d1
Enei <eneipereira@outlook.com.br>
2022-09-14 18:31:30 -0300

    feat: Cria a interface "IVehicle" de forma genérica com os atributos correspondentes (Requisito 2)

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const Vehicle = z.object({
+  model: z.string().min(3),
+  year: z.number().int().positive().gte(1900)
+    .lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof Vehicle>;
+
+export { Vehicle };
