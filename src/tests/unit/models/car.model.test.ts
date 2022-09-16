4ddae24f15d8f0a803e8e2f739f6e17d12ac5761
Enei <eneipereira@outlook.com.br>
2022-09-16 16:17:53 -0300

    test: Implementa testes para a camada de Model da rota GET /cars/id

--- src/tests/unit/models/car.model.test.ts
+++ src/tests/unit/models/car.model.test.ts
@@ -1,9 +1,14 @@
 import * as sinon from 'sinon';
-import chai from 'chai';
+import chai, { use } from 'chai';
+import Mongoose from 'mongoose';
 import { Model as CarDAO } from 'mongoose';
 import { allCarsMock, carMock, carMockWithId } from '../../mocks/carMock';
 import CarModel from '../../../models/Car.model';
+import chaiAsPromised from 'chai-as-promised';
+import InvalidMongoIdError from '../../../errors/InvalidMongoIdError';
 const { expect } = chai;
+
+use(chaiAsPromised)
 
 describe('src/models/car.model', () => {
   const carModel = new CarModel();
@@ -11,8 +16,9 @@ describe('src/models/car.model', () => {
   beforeEach(sinon.restore)
 
   describe('create', () => {
-    it('should return a new created item', async () => {
+    it('should return a new created car', async () => {
       sinon.stub(CarDAO, 'create').resolves(carMockWithId)
+      
       const newCar = await carModel.create(carMock)
 
       expect(newCar).to.deep.eq(carMockWithId)
@@ -20,7 +26,7 @@ describe('src/models/car.model', () => {
   })
 
   describe('read', () => {
-    it('should return an array with all itens', async () => {
+    it('should return an array with all cars', async () => {
       sinon.stub(CarDAO, 'find').resolves(allCarsMock)
 
       const cars = await carModel.read()
@@ -28,12 +34,30 @@ describe('src/models/car.model', () => {
       expect(cars[0]).to.deep.eq(allCarsMock[0])
     })
 
-    it('should return an empty array if there aren\'t itens', async () => {
+    it('should return an empty array if there aren\'t cars', async () => {
       sinon.stub(CarDAO, 'find').resolves([])
 
       const cars = await carModel.read()
 
       expect(cars).to.deep.eq([])
+    })
+  })
+
+  describe('readOne', () => {
+    it('should return an object with the right car as result', async () => {
+      sinon.stub(Mongoose, 'isValidObjectId').returns(true)
+      sinon.stub(CarDAO, 'findById').resolves(carMockWithId)
+    
+      const car = await carModel.readOne(carMockWithId._id)
+    
+      expect(car).to.deep.eq(carMockWithId)
+    })
+    
+    it('should throw an InvalidMongoIderror if an invalid id is passed', async () => {
+      sinon.stub(Mongoose, 'isValidObjectId').returns(false)
+      
+      return expect(carModel.readOne('ObjectIdInvalido'))
+        .to.be.eventually.rejectedWith(InvalidMongoIdError)
     })
   })
 });
