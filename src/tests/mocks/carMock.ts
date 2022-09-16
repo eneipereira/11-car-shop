a284320f925f188f2cc481d5b0bcdcc6d56c896a
Enei <eneipereira@outlook.com.br>
2022-09-16 05:39:01 -0300

    feat: Implementa testes  para a camada de Model da rota GET /cars

--- src/tests/mocks/carMock.ts
+++ src/tests/mocks/carMock.ts
@@ -8,11 +8,10 @@ export const carMock = {
 }
 
 export const carMockWithId = {
+  ...carMock,
   _id: "4edd40c86762e0fb12000003",
-  model: "Ferrari Maranello",
-  year: 1963,
-  color: "red",
-  buyValue: 3500000,
-  seatsQty: 2,
-  doorsQty: 2
 }
+
+export const allCarsMock = [
+  { ...carMockWithId }
+]
--- src/tests/unit/models/car.model.test.ts
+++ src/tests/unit/models/car.model.test.ts
@@ -1,7 +1,7 @@
 import * as sinon from 'sinon';
 import chai from 'chai';
 import { Model as CarDAO } from 'mongoose';
-import { carMock, carMockWithId } from '../../mocks/carMock';
+import { allCarsMock, carMock, carMockWithId } from '../../mocks/carMock';
 import CarModel from '../../../models/Car.model';
 const { expect } = chai;
 
@@ -17,5 +17,23 @@ describe('src/models/car.model', () => {
 
       expect(newCar).to.deep.eq(carMockWithId)
     });
+  })
+
+  describe('read', () => {
+    it('should return an array with all itens', async () => {
+      sinon.stub(CarDAO, 'find').resolves(allCarsMock)
+
+      const cars = await carModel.read()
+
+      expect(cars[0]).to.deep.eq(allCarsMock[0])
+    })
+
+    it('should return an empty array if there aren\'t itens', async () => {
+      sinon.stub(CarDAO, 'find').resolves([])
+
+      const cars = await carModel.read()
+
+      expect(cars).to.deep.eq([])
+    })
   })
 });
